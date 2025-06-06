b48b2e1ebef772fcf5f45e2a2ffac2daf606b32c
System Administrator <root@rudiminis-Mac-mini.local>
2025-06-06 14:50:59 +0200

    Update server.js and cleanup

--- backend/server.js
+++ backend/server.js
@@ -206,6 +206,8 @@ app.get('/api/limits', auth, async (req, res) => {
   }
 });
 
+
+
 // POST to save limits + lock state
 app.post('/api/limits', auth, async (req, res) => {
   const { ubs, corner, pfm, cembra, locked } = req.body;
