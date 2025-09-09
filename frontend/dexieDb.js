b0378ea31227dd9d32058812e3a913fffe1974e4
System Administrator <root@rudiminis-Mac-mini.local>
2025-09-09 10:16:58 +0200

    update

--- frontend/dexieDb.js
+++ frontend/dexieDb.js
@@ -13,7 +13,7 @@ const backend =
 export const db = new Dexie('bookkeeping-db');
 
 // Always keep the highest version here
-db.version(308).stores({
+db.version(309).stores({
   entries: '_id, date, amount, category, person, bank, synced, lastUpdated, note',
   notes: '_id, title, content, done, synced, lastUpdated',
   balances: 'bank',
